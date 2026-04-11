4ba065620b31be120ae546aa485aa572ede47e6f
pajanekkk <phovjadsky@gmail.com>
2026-04-12 00:55:50 +0200

    .

--- packages/student/src/index.js
+++
@@ -1,8 +0,0 @@
-// export * as GroupGQLModel from "./GroupGQLModel"
-// export * as UserGQLModel from "./UserGQLModel"
-// export * as RoleGQLModel from "./RoleGQLModel"
-// export * as MembershipGQLModel from "./MembershipGQLModel"
-
-// export * as GroupTypeGQLModel from "./GroupTypeGQLModel"
-// export * as RoleTypeGQLModel from "./RoleTypeGQLModel"
-export * from "./Base"
